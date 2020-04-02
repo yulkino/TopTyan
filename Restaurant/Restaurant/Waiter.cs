ca4e01b51887d4d9e15d34c78fa8371509e98a0c
yulkino <yulkino.chan.desu@gmail.com>
2020-04-02 23:03:54 +0500

    waiter0

---
+++ Restaurant/Restaurant/Waiter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    class Waiter
+    {
+        private static List<int> NoteBook = new List<int>();
+
+        public Waiter() 
+        {
+
+        }
+    }
+}
